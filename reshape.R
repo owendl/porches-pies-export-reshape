bd7d46676092b03427fd2c2da2e9b3cd293803a3
Drew Owen <owen.drew.l@gmail.com>
2022-11-19 11:12:46 -0500

    Added split export and extract_baker_category from Product.Options

--- reshape.R
+++ reshape.R
@@ -12,3 +12,18 @@ clean_up_export<-function(df){
     drop_na(Product.Id)
 return(df_new)  
 }
+
+split_export<-function(df){
+  bakers = df[df$Product.Name == "Bake-Off Entry",]
+  items = df[df$Product.Name != "Bake-Off Entry",]
+  return(list(bakers,items))
+}
+
+extract_baker_category<-function(df){
+  regex_string = '(: [[:alnum:]-\\s]+)'
+  df_new = df %>% 
+    extract(Product.Options,"baker_info", regex_string) %>% 
+    separate(baker_info,c("proam", "savsweet"), sep = " - ") %>% 
+    extract(proam, "proam", "([A-Za-z]+)")
+  return(df_new)
+}
